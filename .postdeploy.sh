31e39987fd801b686a27ace19ab1c350b9c5fdc4
macschlingel <info@schlingel.de>
2015-10-08 18:18:08 +0200

    added slack notification

--- .postdeploy.sh
+++ .postdeploy.sh
@@ -1,5 +1,10 @@
+source ../../data/postdeploy.vars.sh
+
 echo "Removing old content dirs/links just to make sure..."
 rm -Rf content
 
 echo "Creating a new link to content dir..."
 ln -s ../../../../repos/slowcarber-content content
+
+echo "Notifying Slack"
+curl -X POST --data-urlencode 'payload={"channel": "#slowcarber", "username": "webserver", "text": "Changes receid. Now live."}' $slackURL
