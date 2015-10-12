013c0dd573f24a0509ae44a1236001aee6e597c8
macschlingel <info@schlingel.de>
2015-10-12 09:13:29 +0200

    adding UTF-8 again, now with correct brackets

--- site/snippets/podcastfeed.php
+++ site/snippets/podcastfeed.php
@@ -23,7 +23,8 @@ if($enablePodtrac){
     xmlns:dc="http://purl.org/dc/elements/1.1/"
     xmlns:atom="http://www.w3.org/2005/Atom"
     xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd"
-    xml:lang="de-DE"?>
+    xml:lang="de-DE"
+	encoding="UTF-8">
 
   <channel>
     <title><?php echo (isset($title)) ? xml($title) : xml($page->title()) ?></title>
